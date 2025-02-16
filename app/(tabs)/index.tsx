2ec13323c3d2f82f93a1d23df9b772d53d593ceb
M-Harley01 <190006115@dundee.ac.uk>
2025-02-16 22:41:34 +0000

    few changes made

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,58 @@
-/* index.tsx */ 
-
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { ThemedView } from '@/components/ThemedView';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function ProfileScreen() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    { label: 'January', value: 'January' },
+    { label: 'February', value: 'February' },
+    { label: 'March', value: 'March' },
+    { label: 'April', value: 'April' },
+    { label: 'May', value: 'May' },
+    { label: 'June', value: 'June' },
+    { label: 'July', value: 'July' },
+    { label: 'August', value: 'August' },
+    { label: 'September', value: 'September' }, 
+    { label: 'October', value: 'October' },
+    { label: 'November', value: 'November' },
+    { label: 'December', value: 'December' }
+  ]);
+
   return (
-    <ThemedView style={styles.container}>
-      <Text style={styles.text}>Welcome to the index Screen!</Text>
-    </ThemedView>
+    <View style={styles.mainView}>
+      <Text style={styles.headingText}>Colleague Profile</Text>
+      <Text style={styles.text}>Name: firstName surname</Text>
+      <Text style={styles.text}>Store Location: Dundee</Text>
+      <Text style={styles.text}>Holiday days:</Text>
+      <Text style={styles.text}>Request Holidays:</Text>
+
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  mainView: {
+    padding: 45
+  },
+  headingText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: "#ffffff",
+    paddingBottom: 45,
   },
   text: {
-    fontSize: 20,
+    fontSize: 30,
     fontWeight: 'bold',
-    color: "#ffffff"
+    color: "#ffffff",
   },
 });
