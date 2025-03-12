00cfa4a405be1bd2e23496358825179bbbce2d9f
M-Harley01 <190006115@dundee.ac.uk>
2025-03-12 12:49:37 +0000

    Beginning work on allowing users to take a picture wile using the camera

--- app/attachImage.tsx
+++ app/attachImage.tsx
@@ -1,21 +1,25 @@
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
+
+import { AntDesign } from '@expo/vector-icons';
+import { CameraType, CameraView, useCameraPermissions } from 'expo-camera';
+import { useRef, useState } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
+export default function Camera() {
   const [facing, setFacing] = useState<CameraType>('back');
   const [permission, requestPermission] = useCameraPermissions();
+  const [photo, setPhoto] = useState<any>(null);
+  const cameraRef = useRef<CameraView | null>(null);
 
   if (!permission) {
-    
+  
     return <View />;
-  } 
+  }
 
   if (!permission.granted) {
     
     return (
       <View style={styles.container}>
-        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
         <Button onPress={requestPermission} title="grant permission" />
       </View>
     );
@@ -25,48 +29,61 @@ export default function App() {
     setFacing(current => (current === 'back' ? 'front' : 'back'));
   }
 
+  const handleTakePhoto =  async () => {
+    if (cameraRef.current) {
+        const options = {
+            quality: 1,
+            base64: true,
+            exif: false,
+        };
+        const takedPhoto = await cameraRef.current.takePictureAsync(options);
+
+        setPhoto(takedPhoto);
+    }
+  }; 
+
+  
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}/>
+      <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Flip Camera</Text>
+            <AntDesign name='retweet' size={44} color='black' />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={handleTakePhoto}>
+            <AntDesign name='camera' size={44} color='black' />
           </TouchableOpacity>
         </View>
-
+      </CameraView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'flex-start',
-      alignItems: 'center', 
-    },
-    message: {
-      textAlign: 'center',
-      paddingBottom: 10,
-    },
-    camera: {
-      width: '100%', 
-      height: '70%', 
-    },
-    buttonContainer: {
-      position: 'absolute',
-      bottom: 20, 
-      alignSelf: 'center',
-      paddingBottom: '10%'
-    },
-    button: {
-      backgroundColor: "white",
-      padding: 10,
-      borderRadius: 5,
-    },
-    text: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      color: 'black',
-    },
-  });
-  
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    backgroundColor: 'gray',
+    borderRadius: 10,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
